fcda6a880a14af4527fb826afbf53f68d7e9bd60
Lucas Araujo <lucasneedbon@gmail.com>
2022-04-05 09:40:24 -0300

    Acessando-site

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from selenium import webdriver
 
 os.environ['PATH'] += r"C:\workspace\py-se-booking"
-diver = webdriver.Chrome()
-
-
+driver = webdriver.Chrome()
+driver.get('https://www.myinstants.com/instant/to-be-continued-jojo/')
+my_element = driver.find_element_by_id('instant-page-button')
+my_element.click()

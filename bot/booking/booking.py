35fb643d65a04a5de15e0c278e4d52cf694d592b
Lucas Araujo <lucasneedbon@gmail.com>
2022-04-06 19:17:44 -0300

    seletores

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -4,10 +4,13 @@ from selenium import webdriver
 
 
 class Booking(webdriver.Chrome):
-    def __init__(self, driver_path=r"C:\workspace\py-se-booking"):
+    def __init__(self, driver_path=r"C:\workspace\py-se-booking", teardown=False):
         self.driver_path = driver_path
         os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.quit()
 
     def land_first_page(self):
         self.get(BASE_URL)
--- bot/run.py
+++ bot/run.py
@@ -1,5 +1,6 @@
 from booking.booking import Booking
 
 
-inst = Booking()
-inst.land_first_page()
+with Booking() as bot:
+    bot.land_first_page()
+    print('Saindo...')
